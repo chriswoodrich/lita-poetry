7a78b1533b1e8c978a7e1597df5e94e353405f5c
Chris Woodrich <cwoodrich@gmail.com>
2015-03-08 00:59:27 -0800

    Add test for non haikus

--- spec/lita/handlers/poetry_spec.rb
+++ spec/lita/handlers/poetry_spec.rb
@@ -16,6 +16,24 @@ describe Lita::Handlers::Poetry, lita_handler: true do
 
       expect(replies.last).to eq('Garth, that was a haiku!')
     end
+
+    it 'does nothing when 3 consective lines do not have a 5-7-5 pattern' do
+
+      send_message("Friends, Romans, countrymen, lend me your ears;")
+      send_message("I come to bury Caesar, not to praise him.")
+      send_message("The evil that men do lives after them;")
+      send_message("The good is oft interred with their bones;")
+      send_message("So let it be with Caesar. The noble Brutus")
+      send_message("Hath told you Caesar was ambitious:")
+      send_message("If it were so, it was a grievous fault,")
+      send_message("And grievously hath Caesar answer’d it.")
+
+      expect(replies.size).to eq(0)
+    end
+
+
+
+
   end
 
 
